2265f691215b0ed04ce0b3e8af419bfb6315e608
AdityaKonduri79 <80462791+AdityaKonduri79@users.noreply.github.com>
2021-03-27 10:57:20 +0530

    Add files via upload

---
+++ MainProject/src/main/java/com/mainproject/riskdiscovery/base/LoginPage.java
@@ -0,0 +1,5 @@
+package com.mainproject.riskdiscovery.base;
+
+public class LoginPage {
+
+}
---
+++ MainProject/src/main/java/com/mainproject/riskdiscovery/utils/DateUtils.java
@@ -0,0 +1,18 @@
+package com.mainproject.riskdiscovery.utils;
+
+import java.util.Date;
+
+public class DateUtils {
+	
+//	public String[] convertToXpath(String startDate,String endDate){
+//		date = new Date();
+//		String[] st = new String[2];
+//		st[0] = "//*[@aria-label='Selected as start date. "+ date.toString() +"']"; 
+//		return st;
+//	}
+	
+	public static String dateFormatstamp(){
+		Date date = new Date();
+		return date.toString().replaceAll(":", "_").replaceAll(" ", "_");
+	}
+}
